bba16a3a6380bc82ecf45894001861cee25a6791
Tshepiso <timarape.gmail.com>
2020-05-13 06:44:40 +0200

    Upated Card processing

--- BasicProjects/CreditCardApplication/CardApplication.View/MainWindow.xaml.cs
+++ BasicProjects/CreditCardApplication/CardApplication.View/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CardApplication.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,14 +22,51 @@ namespace CardApplication.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Employee employee;
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void buttonApply_Click(object sender, RoutedEventArgs e)
         {
+            
+
+            employee = new Employee()
+            {
+                ApplicationNumber = long.Parse(textBoxApplicationNumber.Text),
+                Salary = double.Parse(textBoxSalary.Text),
+                Rent = double.Parse(textRent.Text),
+                YearsEmployed = int.Parse(textYearEmployed.Text),
+                YearsResiding = int.Parse(textBoxYearsResiding.Text)
+            };
+    
+
+            if (ProcessApplicationSucceds(employee))
+                MessageBox.Show("Your application was succcessful");
+            else
+                MessageBox.Show("Application failed");
+            
+        }
+
+        private bool ProcessApplicationSucceds(Employee employee)
+        {
+            bool isEligible = false;
+            double minimumRent = 0.25 * employee.Salary;
+
+            if(employee.Salary > 2500  && 
+               employee.Rent < minimumRent && 
+               employee.YearsResiding > 5 && 
+               employee.YearsEmployed >3)
+            {
+                isEligible = true;
+            }
+
+            return isEligible;
 
         }
     }
 }
+
+    
