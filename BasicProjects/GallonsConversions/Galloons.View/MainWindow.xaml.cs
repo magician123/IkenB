59f612c0b9024837d92b2be7de7cdc86943eb2d9
Tshepiso <timarape.gmail.com>
2020-05-13 17:24:00 +0200

    Gallons Converter basic app

---
+++ BasicProjects/GallonsConversions/Galloons.View/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Galloons.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void convertGallons_Click(object sender, RoutedEventArgs e)
+        {
+            int litres = 20;
+
+            for (int i =litres ; i <= 60; i++)
+            {
+                var gallons = i * 4;
+
+                listBoxResults.Items.Add($"for {i} gallons we have {gallons} litres");
+
+                i += 4;
+            }
+        }
+    }
+}
